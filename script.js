b4d4c5c7682619ec6cd6c75c6b976a09e54d7062
Antoniolps <94399129+Antoniolps@users.noreply.github.com>
2021-11-20 20:57:55 -0300

    Add files via upload
    Mudanças no JS
    Inicio da parte de php
    Html linkado com o php

--- script.js
+++ script.js
@@ -4,42 +4,55 @@ const matricula = document.getElementById('matricula');
 const pri_nota = document.getElementById('pri_nota');
 const seg_nota = document.getElementById('seg_nota');
 
-form.addEventListener('submit', e => {
-	e.preventDefault();
-	checkInputs();
-});
-
-function checkInputs() /* function que checa todos os campos */
-{
+form.addEventListener('submit', function checkInputs(event){ /* function que checa todos os campos */
 	const nomeValue = nome.value.trim();
 	const matriculaValue = matricula.value.trim();
 	const pri_notaValue = pri_nota.value.trim();
 	const seg_notaValue = seg_nota.value.trim();
 	
-	if(nomeValue === '') {
+	if(nomeValue == '') {
+		event.preventDefault();
 		setErrorFor(nome, 'Preencha o campo em branco!');
 	} else {
 		setSuccessFor(nome);
 	}
 	
-	if(matriculaValue === '') {
+	if(matriculaValue == '') {
+		event.preventDefault();
 		setErrorFor(matricula, 'Preencha o campo em branco!');
 	} else {
 		setSuccessFor(matricula);
 	}
 	
-	if(pri_notaValue === '') {
+	if(pri_notaValue == '') {
+		event.preventDefault();
 		setErrorFor(pri_nota, 'Preencha o campo em branco!');
 	} else {
 		setSuccessFor(pri_nota);
 	}
 	
-	if(seg_notaValue === '') {
+	if(seg_notaValue == '') {
+		event.preventDefault();
 		setErrorFor(seg_nota, 'Preencha o campo em branco!');
 	} else {
 		setSuccessFor(seg_nota);
 	}
-}
+	if(isNaN(pri_notaValue)){
+		event.preventDefault();
+		setErrorFor(pri_nota, 'Digite um numero válido!');
+	}
+	else{
+		setSuccessFor(pri_nota);
+	}
+	if(isNaN(seg_notaValue)){
+		event.preventDefault();
+		setErrorFor(seg_nota, 'Digite um numero válido!');
+	}
+	else{
+		setSuccessFor(seg_nota);
+	}
+
+});
 
 function setErrorFor(input, message) {
 	const formControl = input.parentElement;
